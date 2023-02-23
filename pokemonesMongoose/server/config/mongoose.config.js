5c0d14c8fedf099d487431a20305081377e73149
Danilorca <daniela.lorca.r@gmail.com>
2023-02-23 17:51:46 -0300

    CRUD Pokemones

---
+++ pokemonesMongoose/server/config/mongoose.config.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost/pokemonesMongooseDB",{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+.then(()=>console.log("BASE DE DATOS CONECTADA"))
+.catch(err=>console.log("Algo salio mal",err))
+
+
+
